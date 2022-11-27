321220b840c21b860e14e1cd0961a9ed5f58a927
Aboubacar Sidiki Douno <douno30@gmail.com>
2022-11-27 13:23:10 +0100

    Set up react bootstrap

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+        <Container>
+            <Row>
+                <Col className='text-center py-3'>Copyright &copy; App Name</Col>
+            </Row>
+        </Container>
+    </footer>
   )
 }
 
-export default Footer
+export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Navbar, Nav, Container, Row } from 'react-bootstrap';
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="#home">App Name</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/page-1"><i className='fas fa-shopping-cart'></i>Page 1</Nav.Link>
+              <Nav.Link href="/page-2"><i className='fas fa-user'></i>Page 2</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   )
 }
 
-export default Header
+export default Header;
